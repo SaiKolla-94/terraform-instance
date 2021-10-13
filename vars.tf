d049e1ec2659fa30bdaabf6f614872e607d25aca
SaiKolla <56868273+SaiKolla-94@users.noreply.github.com>
2021-10-13 16:09:00 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -14,6 +14,6 @@ variable "AMIS" {
   default = {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-041d6256ed0f2061c"
+    eu-west-1 = "ami-0110a207d45daf145"
   }
 }
